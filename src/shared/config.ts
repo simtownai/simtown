4b6c972be7c6b259577112e5f0af7db79b261f74
Alexander Smirnov <alex@asmirnov.xyz>
2024-09-24 21:11:24 -0300

    add config and types

---
+++ src/shared/config.ts
@@ -0,0 +1,4 @@
+export const CONFIG = {
+  SERVER_PORT: 3000,
+  SERVER_URL: "http://localhost:3000",
+}
---
+++ src/shared/types.ts
@@ -0,0 +1,5 @@
+export interface PlayerPosition {
+  id: string
+  x: number
+  y: number
+}
