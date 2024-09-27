85590afc5a46934984695f2ddcf8f9f198f2c0a3
Alexander Smirnov <alex@asmirnov.xyz>
2024-09-27 01:20:12 -0300

    add phaser

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,18 @@
-export default {}
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      phaser: "phaser/dist/phaser.js",
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ["phaser"],
+        },
+      },
+    },
+  },
+})
