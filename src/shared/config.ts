2905abe215a0ad2881270fd977b9c2c3993798a4
Alexander Smirnov <alex@asmirnov.xyz>
2024-09-29 20:42:11 -0300

    change server url back

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   SERVER_PORT: 3000,
-  SERVER_URL: "http://localhost:3000",
+  SERVER_URL: "https://api.simtown.dev",
   SPRITE_WIDTH: 100,
   SPRITE_CHARACTER_WIDTH: 16,
 }
