2875ed81f447c8107a793244c34eb342e4662f33
Alexander Smirnov <alex@asmirnov.xyz>
2024-10-16 12:47:05 -0300

    change date calculation

--- src/shared/functions.ts
+++ src/shared/functions.ts
@@ -1,16 +1,21 @@
 import { CONFIG } from "./config"
 import { PlayerSpriteDefinition } from "./types"
 
-export function getTime(): Date {
-  const currentDate = new Date() // Real current time
-  const timeDifferenceMs = currentDate.getTime() - CONFIG.START_DATE.getTime() // Time difference in milliseconds
+const currentDate = new Date()
+const realTimeDifferenceMs = CONFIG.TARGET_DATE.getTime() - currentDate.getTime()
+const startDateMs = CONFIG.TARGET_DATE.getTime() - realTimeDifferenceMs / 60
 
-  // Multiply the time difference by 60 to speed up the in-game time
+// Time calculation function
+export function getTime(): Date {
+  const currentDate = new Date()
+
+  const timeDifferenceMs = currentDate.getTime() - startDateMs
+
+  // Scale the difference by 60 for in-game time progression
   const gameTimeDifferenceMs = timeDifferenceMs * 60
 
-  // Calculate the in-game date by adding the adjusted time difference to the start date
-  const inGameDate = new Date(CONFIG.START_DATE.getTime() + gameTimeDifferenceMs)
-
+  // Calculate and return the in-game date
+  const inGameDate = new Date(startDateMs + gameTimeDifferenceMs)
   return inGameDate
 }
 
