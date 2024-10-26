70ecb5b25e023eaeea5bd04c6ba47cdf71750ea9
Szymon Zmyslony <szymon.zmyslony@gmail.com>
2024-10-26 16:01:47 +0100

    Fix format

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -84,7 +84,6 @@ function App() {
       })
     })
 
-
     newSocket.on("endConversation", (message: ChatMessage) => {
       setMessages((prevMessages) => {
         const oldMessages = prevMessages.get(message.from) || []
