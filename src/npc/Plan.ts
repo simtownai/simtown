cf56fcf6951d6a76b51f705544fe8e9931720a99
Szymon Zmyslony <szymon.zmyslony@gmail.com>
2024-10-26 10:39:54 +0100

    Validate people and places in plan

--- src/npc/Plan.ts
+++ src/npc/Plan.ts
@@ -3,26 +3,83 @@ import { NPC } from "./client"
 import client from "./openai"
 import { planning_prompt } from "./prompts"
 import { zodResponseFormat } from "openai/helpers/zod.mjs"
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs"
 import { z } from "zod"
 
 const ResponseSchema = z.object({
   plan: ActionPlanSchema,
 })
 
+const validateActions = (
+  plan: ActionPlan,
+  places: string[],
+  playerNames: string[],
+): { isValid: boolean; error?: string } => {
+  for (const action of plan) {
+    if (action.type === "move" && action.target.targetType !== "coordinates") {
+      if (action.target.targetType === "place" && !places.includes(action.target.name)) {
+        return {
+          isValid: false,
+          error: `Invalid place: ${action.target.name} for the move action. Only ${places.join(", ")} are available`,
+        }
+      }
+      if (action.target.targetType === "person" && !playerNames.includes(action.target.name)) {
+        return {
+          isValid: false,
+          error: `Invalid person ${action.target.name} for the move action. Only ${playerNames.join(
+            ", ",
+          )} are available`,
+        }
+      }
+    }
+    if (action.type === "talk" && !playerNames.includes(action.name)) {
+      return {
+        isValid: false,
+        error: `Invalid person for talk: ${action.name}. Only ${playerNames.join(", ")} are available`,
+      }
+    }
+  }
+  return { isValid: true }
+}
+
 export const generatePlanForTheday = async (npc: NPC): Promise<ActionPlan> => {
   const npcBackground = npc.aiBrain.getNPCMemories()
+  const playerNames = Array.from(npc.otherPlayers.keys())
+  const places = npc.objectLayer!.map((obj) => obj.name)
+  let attemps = 0
+  let maxAttemps = 5
+
+  let isValid = false
+  let errorMessage = ""
+  let plan: ActionPlan
+
   const prompt = planning_prompt(npcBackground)
+  let messages = [{ role: "system", content: prompt }] as ChatCompletionMessageParam[]
 
-  const completion = await client.beta.chat.completions.parse({
-    model: "gpt-4o-mini",
-    messages: [{ role: "system", content: prompt }],
-    response_format: zodResponseFormat(ResponseSchema, "plan"),
-  })
+  while (attemps < maxAttemps) {
+    attemps++
+    const completion = await client.beta.chat.completions.parse({
+      model: "gpt-4o-mini",
+      messages: messages,
+      response_format: zodResponseFormat(ResponseSchema, "plan"),
+    })
 
-  const parsed = completion.choices[0].message.parsed
-  if (!parsed) {
-    throw new Error("Couldn't create a plan")
+    const parsed = completion.choices[0].message.parsed
+    if (!parsed) {
+      throw new Error("Couldn't create a plan")
+    }
+
+    plan = parsed.plan
+    const validation = validateActions(plan, places, playerNames)
+    isValid = validation.isValid
+    errorMessage = validation.error || ""
+    if (!isValid) {
+      messages.push({ role: "system", content: errorMessage })
+      console.error("Invalid plan, retrying...", errorMessage)
+    }
+    if (isValid) {
+      return plan
+    }
   }
-  console.log("Generated plan for the day:", parsed.plan)
-  return parsed.plan
+  throw new Error("Couldn't create a plan")
 }
