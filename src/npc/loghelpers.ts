e9ef4854a8cbdbe348ca521f6f2c845f673f0b7f
Szymon Zmyslony <szymon.zmyslony@gmail.com>
2024-11-03 19:26:08 +0000

    Thread logs with debug

--- src/npc/loghelpers.ts
+++ src/npc/loghelpers.ts
@@ -3,15 +3,15 @@ import { BrainDump } from "./brain/AIBrain"
 
 export const log_threads = (aiBrainDumop: BrainDump) => {
   const conversations = aiBrainDumop.conversations.threads
-  logger.error(`(${aiBrainDumop.playerData.username})`)
+  logger.debug(`(${aiBrainDumop.playerData.username})`)
   for (const conversation of conversations) {
     const playerName = conversation[0]
-    console.error(`Threads with ${playerName}`)
+    console.debug(`Threads with ${playerName}`)
     const threadMeat = conversation[1]
     for (const thread of threadMeat) {
-      console.error("Finished", thread.finished)
-      console.error("AI messages", thread.aiMessages)
-      console.error("Chat messages", thread.messages)
+      console.debug("Finished", thread.finished)
+      console.debug("AI messages", thread.aiMessages)
+      console.debug("Chat messages", thread.messages)
     }
   }
 }
