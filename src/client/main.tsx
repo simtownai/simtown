79a30e23fcffdd753407ffd8e6313306a0139c2b
Alexander Smirnov <alex@asmirnov.xyz>
2024-11-21 18:07:40 -0300

    upd posthog config to capture canvas

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -5,13 +5,20 @@ import { PostHogProvider } from "posthog-js/react"
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const options = {
-  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        session_recording: {
+          captureCanvas: {
+            recordCanvas: true,
+            canvasFps: 10,
+          },
+        },
+      }}
+    >
       <App />
     </PostHogProvider>
   </React.StrictMode>,
