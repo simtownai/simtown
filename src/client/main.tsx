d0dc5f22b42e889440f4990196d156187a143a95
Szymon Zmyslony <szymon.zmyslony@gmail.com>
2024-11-21 16:13:23 +0000

    add posthog

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,6 +2,14 @@ import App from "./App"
 import "./style.css"
 import React from "react"
 import ReactDOM from "react-dom/client"
+import posthog from 'posthog-js'
+
+posthog.init('phc_zsXZZdjyYnsaJHkTLE66nFDoaYVR5OEfVivZ20WCpsQ',
+  {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+  }
+)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
