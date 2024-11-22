272ffd611260f36845b935bcc1abef7745194e08
Szymon Zmyslony <szymon.zmyslony@gmail.com>
2024-11-22 16:46:50 +0000

    turn off npcs

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -6,7 +6,7 @@ export const CONFIG = {
   SERVER_PORT: 3000,
   SERVER_URL: isDevelopment ? "http://localhost:3000" : "https://api.simtown.ai",
   // this is a debug flag that will make the AI Stop reflecting and get stuck after finishing any move action
-  ENABLE_NPC_AUTOMATION: isDevelopment ? true : true,
+  ENABLE_NPC_AUTOMATION: isDevelopment ? true : false,
   TILE_SIZE: 16,
   SPRITE_WIDTH: 16,
   SPRITE_HEIGHT: 32,
