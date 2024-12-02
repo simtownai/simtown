d7e6d74a256794b148dbe726ab872d7351f064c3
Alexander Smirnov <alex@asmirnov.xyz>
2024-12-01 20:29:39 -0500

    move roomconfig to shared for client map use

--- src/shared/roomConfig.ts
+++ src/shared/roomConfig.ts
@@ -5,6 +5,7 @@ import {
   murderdronesNPCConfigs,
 } from "../npc/npcConfig"
 import { characterAIPromptSystem, electiontownPromptSystem, harryPromptSystem } from "../npc/prompts"
+import { electiontownMap, harrygobletoffireMap } from "../shared/maps"
 import { RoomConfig } from "../shared/types"
 
 export const roomsConfig: RoomConfig[] = [
@@ -13,23 +14,27 @@ export const roomsConfig: RoomConfig[] = [
     instanceType: "shared",
     NPCConfigs: electiontownNPCConfigs,
     promptSystem: electiontownPromptSystem,
+    mapConfig: electiontownMap,
   },
   {
     path: "characterai",
     instanceType: "private",
     NPCConfigs: characteraiNPCConfigs,
     promptSystem: characterAIPromptSystem,
+    mapConfig: electiontownMap,
   },
   {
     path: "murderdrones",
     instanceType: "private",
     NPCConfigs: murderdronesNPCConfigs,
     promptSystem: characterAIPromptSystem,
+    mapConfig: electiontownMap,
   },
   {
     path: "harry",
     instanceType: "private",
     NPCConfigs: harryNPCConfigs,
     promptSystem: harryPromptSystem,
+    mapConfig: harrygobletoffireMap,
   },
 ]
