0889d24243afe0296c2104f4d3d2ea5adfa6f5d3
Szymon Zmyslony <szymon.zmyslony@gmail.com>
2024-12-09 21:13:15 +0100

    Move supabase auth into hook

---
+++ src/client/hooks/useSupabaseSession.ts
@@ -0,0 +1,57 @@
+import { PlayerSpriteDefinition } from "../../shared/types"
+import { supabase } from "../supabase"
+import { Session } from "@supabase/supabase-js"
+import { useEffect, useState } from "react"
+import { Socket } from "socket.io-client"
+
+interface UseSupabaseSessionReturn {
+  supabaseSession: Session | null
+  username: string
+  spriteDefinition: PlayerSpriteDefinition
+}
+
+export const useSupabaseSession = (
+  defaultUsername: string,
+  defaultSpriteDefinition: PlayerSpriteDefinition,
+  socket: Socket,
+): UseSupabaseSessionReturn => {
+  const [supabaseSession, setSupabaseSession] = useState<Session | null>(null)
+  const [username, setUsername] = useState<string>(defaultUsername)
+  const [spriteDefinition, setSpriteDefinition] = useState<PlayerSpriteDefinition>(defaultSpriteDefinition)
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange(async (event, session) => {
+      console.log("onAuthStateChange", event, session)
+      setSupabaseSession(session)
+      if (session) {
+        setUsername(session.user.email ? session.user.email.split("@")[0] : session.user.id)
+        supabase
+          .from("users")
+          .select("sprite_definition")
+          .then(({ data, error }) => {
+            if (error) {
+              console.error("Error loading user sprite definition:", error)
+              return
+            }
+            const spriteDefinition = data[0].sprite_definition
+            if (spriteDefinition) {
+              setSpriteDefinition(spriteDefinition as PlayerSpriteDefinition)
+            }
+          })
+        socket?.emit("authorizeSupabase", session.access_token)
+      }
+    })
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [socket])
+
+  return {
+    supabaseSession,
+    username,
+    spriteDefinition,
+  }
+}
