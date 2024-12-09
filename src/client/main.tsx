5ef495d326c3aa2a1a075984d40bc2a17d68dc78
Alexander Smirnov <alex@asmirnov.xyz>
2024-12-08 20:54:23 -0500

    rm double rendering on dev

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -6,20 +6,20 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <PostHogProvider
-      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
-      options={{
-        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-        session_recording: {
-          captureCanvas: {
-            recordCanvas: true,
-            canvasFps: 10,
-          },
+  //<React.StrictMode>
+  <PostHogProvider
+    apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+    options={{
+      api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+      session_recording: {
+        captureCanvas: {
+          recordCanvas: true,
+          canvasFps: 10,
         },
-      }}
-    >
-      <App />
-    </PostHogProvider>
-  </React.StrictMode>,
+      },
+    }}
+  >
+    <App />
+  </PostHogProvider>,
+  // </React.StrictMode>,
 )
