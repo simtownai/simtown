3968c2b56703b0f6e27a0003b1473bbf09caa7c0
Alexander Smirnov <alex@asmirnov.xyz>
2024-12-18 08:28:18 -0500

    change prod model to gpt-4o-mini

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -3,7 +3,7 @@ export const isDevelopment = process.env.NODE_ENV !== "production"
 export const CONFIG = {
   SERVER_PORT: 3000,
   SERVER_URL: isDevelopment ? "http://localhost:3000" : "https://api.simtown.ai",
-  MODEL_NAME: isDevelopment ? "gpt-4o-mini" : "gpt-4o",
+  MODEL_NAME: isDevelopment ? "gpt-4o-mini" : "gpt-4o-mini",
   // this is a debug flag that will make the AI Stop reflecting and get stuck after finishing any move action
   ENABLE_NPC_AUTOMATION: isDevelopment ? true : true,
   TILE_SIZE: 16,
